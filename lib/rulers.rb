b0ee42983c212c3bdbf5c8ec0f6facfafe47bb1f
Amanda Ross <amikaross@gmail.com>
2023-03-29 16:56:47 -0600

    Redirecting if path is '/' experiment

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,13 +8,23 @@ module Rulers
   class Application
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        return[404, {'Content-Type' => 'text/html'}, []]
+        return [404, {'Content-Type' => 'text/html'}, []]
       end
-      klass, act = get_controller_and_action(env)
+      if env['PATH_INFO'] == '/'
+        klass, act = get_controller_and_action({"PATH_INFO" => "/quotes/a_quote"})
+      else
+        klass, act = get_controller_and_action(env)
+      end
       controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
+      begin 
+        text = controller.send(act)
+      rescue RuntimeError
+        [500, {'Content-Type' => 'text/html'},
+        ["Oh no! There's been a runtime error!"]]
+      else
+        [200, {'Content-Type' => 'text/html'},
         [text]]
+      end 
     end
   end
 
